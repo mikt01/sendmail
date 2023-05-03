9f4b192b6f6fa17ba76218adf423b829c672755a
duygu <duygu@duygu-MacBook-Air.local>
2023-05-03 19:55:27 +0300

    server completed

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+const cors = require("cors"); //this is a library to config easily cors
+const readline = require('readline');
+
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json())    
+app.use(cors({ origin: true })); // enable origin cors
+
+var user_email = ""
+var user_app_pass = ""
+
+
+app.post("/send-mail",(req,res)=>{
+    console.log("Request get")
+    let email = req.body.email
+    let title = req.body.title
+    let text  = req.body.text
+    let user_mail = req.body.user_mail
+    let user_pass = req.body.user_pass
+    console.log(user_mail,user_pass)
+    // email = "duygudemirpence@gmail.com"
+
+    // Mail içeriği
+    const mailOptions = {
+        from: process.env.MAIL,
+        to: email,
+        subject: title,
+        text: text
+        };
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: user_mail,
+            pass: user_pass
+        }
+    });
+
+    // Mail gönderme işlemi
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Sended ' + info.response);
+        }
+    });
+
+})
+
+
+const server = app.listen(process.env.PORT || 3000 ,(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("Port Started. Port number %d: ", server.address().port);
+
+   
+        
+    }
+})
